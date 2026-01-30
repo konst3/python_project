d2c349e5a85c6271b3d1193325979504f5353f0d
Konstantinos P <konstpapala007@gmail.com>
2026-01-30 22:15:40 +0200

    ui demo working

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -29,9 +29,11 @@ class UI:
         self.f1.pack()
         self.l1 = tk.Label(self.f1, text="Generate the Sequence")
         self.l1.pack()
-        self.s1 = tk.Scale(self.f1, length=500, from_ = 0, to = 5000, resolution=10, orient="horizontal")
-        self.s1.set(100) # Set the default value to 10
-        self.s1.pack()
+        vcmd = (self.f1.register(self.validate_spinbox), "%P") # Command for the verification function
+        self.s1 = tk.Spinbox(self.f1, from_=0, to=10000, width=7, repeatdelay=500, validate="key", validatecommand=vcmd) # TODO: to=utils.seq_limit
+        self.s1.delete(0, tk.END)
+        self.s1.insert(0, "10")   # Set the default value to 10
+        self.s1.pack(side="left", padx=5, pady=5)
         self.b1 = tk.Button(self.f1, text="Generate", command=self.ui_generate_sequence)
         self.b1.pack()
 
@@ -46,9 +48,21 @@ class UI:
         self.b2 = tk.Button(self.f2, text="Sort", command=self.run_sort)
         self.b2.pack()
 
+    # A method that constantly checks if the spinbox input is correct
+    def validate_spinbox(self, value):
+        if (value == ""): return True
+
+        try:
+            value = int(value)
+            return (0 <= value <= 10000)
+        except:
+            return False
+
     def ui_generate_sequence(self):
-        utils.seq = utils.generate_sequence(N=self.s1.get(), min_value=1, max_value=self.s1.get())
-        print(f"DEBUG: Sequence: {utils.seq}")
+        s1_in = int(self.s1.get())
+
+        utils.seq = utils.generate_sequence(N=s1_in, min_value=1, max_value=s1_in)
+        print(f"DEBUG: Generated Sequence: {utils.seq}")
 
     def run_sort(self):
         self.algorithm_choice = self.cb1.get()
